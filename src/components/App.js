3dc0d9aed17fa00211684040a451a006638bd211
Akin <akinkavak1@gmail.com>
2022-02-07 03:26:35 +0300

    Fetch data from an API and set state using useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => setImage(data.message))
+    }, [])
+
+    if (!image) return <p>Loading...</p>; 
+    
+    return  (
+        <img src={image} alt="A Random Dog" />
+    )    
+};
+
+export default App;
